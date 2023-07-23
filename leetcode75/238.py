245181d321cb37693f3e5f1c06330e9e8a478572
Yuxiang Kang <yxkang1912923753@gmail.com>
2023-07-23 03:13:04 -0700

    Update 238.py

--- leetcode75/238.py
+++ leetcode75/238.py
@@ -6,3 +6,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        def ProductOfArray(arr):
+            product = 1
+            for i in range(len(arr)):
+                product *= arr[i]
+            return product
+        product_nums = ProductOfArray(nums)
+        answer = []
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                answer.append(int(product_nums / nums[i]))
+            else:
+                cut = nums[:]
+                del cut[i]
+                print(cut)
+                print(ProductOfArray(cut))
+                answer.append(ProductOfArray(cut))
+
+        print(answer)
+        return answer
+
+
+nums = [-1,1,0,-3,3]
+test = Solution()
+test.productExceptSelf(nums)
